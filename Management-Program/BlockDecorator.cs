aa3d1d4eaf52be4545f2a67d766ff2cde0ecb9bf
Brayden Faulkner <braylane98@gmail.com>
2018-04-05 14:30:41 -0700

    Added size change function

--- Management-Program/BlockDecorator.cs
+++ Management-Program/BlockDecorator.cs
@@ -29,6 +29,10 @@ namespace Management_Program
                 CurLine = CurLine + database.GetElement(i).GetAmount() + ' ';
                 CurLine = CurLine + database.GetElement(i).GetID() + '\n';
                 CurrBlock.Inlines.Add(CurLine);
+                if (CurrBlock.Height < database.Size()*20)
+                {
+                    CurrBlock.Height = database.Size() * 20;
+                }
             }
             return CurrBlock;
         }
