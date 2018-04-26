ac9f1f81eab23e2c652a35cdaa2ae86d6b9d9362
Jon <Jon@DESKTOP-A7E9G5N>
2018-04-26 03:03:25 -0500

    Small Updates to settings

--- Management-Program/SettingsWindow.xaml.cs
+++ Management-Program/SettingsWindow.xaml.cs
@@ -26,6 +26,25 @@ namespace Management_Program
             BlockColor = B;
             WinColor = W;
             InitializeComponent();
+            if (B == "Snow")
+                ListLabel.Content = "Default";
+            else
+                ListLabel.Content = B;
+
+            if (W == Brushes.Snow)
+                WindowLabel.Content = "Default";
+            else if (W == Brushes.White)
+                WindowLabel.Content = "White";
+            else if (W == Brushes.Red)
+                WindowLabel.Content = "Red";
+            else if (W == Brushes.Blue)
+                WindowLabel.Content = "Blue";
+            else if (W == Brushes.Green)
+                WindowLabel.Content = "Green";
+            else if (W == Brushes.Gray)
+                WindowLabel.Content = "Gray";
+            else if (W == Brushes.Yellow)
+                WindowLabel.Content = "Yellow";
         }
 
         private void ConfirmButton_Click(object sender, RoutedEventArgs e)
@@ -49,42 +68,49 @@ namespace Management_Program
         {
             BlockColor = "White";
             DropBox.IsDropDownOpen = false;
+            ListLabel.Content = "White";
         }
 
         private void Red_Click(object sender, RoutedEventArgs e)
         {
             BlockColor = "Red";
             DropBox.IsDropDownOpen = false;
+            ListLabel.Content = "Red";
         }
 
         private void Blue_Click(object sender, RoutedEventArgs e)
         {
             BlockColor = "Blue";
             DropBox.IsDropDownOpen = false;
+            ListLabel.Content = "Blue";
         }
 
         private void Green_Click(object sender, RoutedEventArgs e)
         {
             BlockColor = "Green";
             DropBox.IsDropDownOpen = false;
+            ListLabel.Content = "Green";
         }
 
         private void Gray_Click(object sender, RoutedEventArgs e)
         {
             BlockColor = "Gray";
             DropBox.IsDropDownOpen = false;
+            ListLabel.Content = "Gray";
         }
 
         private void Yellow_Click(object sender, RoutedEventArgs e)
         {
             BlockColor = "Yellow";
             DropBox.IsDropDownOpen = false;
+            ListLabel.Content = "Yellow";
         }
 
         private void Default_Click(object sender, RoutedEventArgs e)
         {
             BlockColor = "Snow";
             DropBox.IsDropDownOpen = false;
+            ListLabel.Content = "Default";
         }
 
         private void DropBox_KeyDown(object sender, KeyEventArgs e)
@@ -104,6 +130,7 @@ namespace Management_Program
             WinColor = Brushes.White;
             this.Background = WinColor;
             WindowBox.IsDropDownOpen = false;
+            WindowLabel.Content = "White";
         }
 
         private void WRed_Click(object sender, RoutedEventArgs e)
@@ -111,6 +138,7 @@ namespace Management_Program
             WinColor = Brushes.Red;
             this.Background = WinColor;
             WindowBox.IsDropDownOpen = false;
+            WindowLabel.Content = "Red";
         }
 
         private void WBlue_Click(object sender, RoutedEventArgs e)
@@ -118,6 +146,7 @@ namespace Management_Program
             WinColor = Brushes.Blue;
             this.Background = WinColor;
             WindowBox.IsDropDownOpen = false;
+            WindowLabel.Content = "Blue";
         }
 
         private void WGreen_Click(object sender, RoutedEventArgs e)
@@ -125,6 +154,7 @@ namespace Management_Program
             WinColor = Brushes.Green;
             this.Background = WinColor;
             WindowBox.IsDropDownOpen = false;
+            WindowLabel.Content = "Green";
         }
 
         private void WGray_Click(object sender, RoutedEventArgs e)
@@ -132,6 +162,7 @@ namespace Management_Program
             WinColor = Brushes.Gray;
             this.Background = WinColor;
             WindowBox.IsDropDownOpen = false;
+            WindowLabel.Content = "Gray";
         }
 
         private void WYellow_Click(object sender, RoutedEventArgs e)
@@ -139,6 +170,7 @@ namespace Management_Program
             WinColor = Brushes.Yellow;
             this.Background = WinColor;
             WindowBox.IsDropDownOpen = false;
+            WindowLabel.Content = "Yellow";
         }
 
         private void WDefault_Click(object sender, RoutedEventArgs e)
@@ -146,6 +178,7 @@ namespace Management_Program
             WinColor = Brushes.Snow;
             this.Background = WinColor;
             WindowBox.IsDropDownOpen = false;
+            WindowLabel.Content = "Default";
         }
     }
 }
